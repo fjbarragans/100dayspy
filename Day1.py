c27183391d8f9e6bd621dad86c54ee9b4dbf45f2
fjbarragans <franciscojbarragans@gmail.com>
2023-01-24 17:43:57 -0600

    Project1 finish

---
+++ Day1.py
@@ -0,0 +1,5 @@
+#Band-name-generator-start
+print("Hello, I am here to create your restaurant's name.")
+city = input("Where were you born? ")
+food = input("What is your favourite food? ")
+print("Your restaurant should be named: " + city + " " + food)
