03f2df6e55e4ef7dabff713e45a3d5c6d5a5a071
fjbarragans <franciscojbarragans@gmail.com>
2023-02-04 12:57:42 -0600

    caesar cipher

---
+++ Day8.py
@@ -0,0 +1,45 @@
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+import os
+def caesar(direction_d, text_t, shift_s):
+    cypher_text = ""
+    if direction_d == "decode":
+        shift_s *= -1
+    for i in text_t:
+        if i in alphabet:
+            position = alphabet.index(i)
+            new_position = (position + shift_s) % len(alphabet)
+            new_letter = alphabet[new_position]
+            cypher_text += new_letter
+        else:
+            cypher_text += i
+    print(f"Here's the {direction_d}d result: {cypher_text}")
+
+restart = True
+while restart:
+    os.system('cls')
+    print(logo)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(direction_d=direction, text_t=text, shift_s=shift)
+    decision = input("Do you want to restart the cipher program?\n")
+    if decision == "no":
+        restart = False
